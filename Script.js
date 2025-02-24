a20e9f9ab37c376cd437148dfaaeb2f350d9a670
Guilherme Martins <Guilhermartins10@gmail.com>
2025-02-24 18:09:11 -0300

    looping

--- Script.js
+++ Script.js
@@ -3,19 +3,21 @@ const tartarugas2 = document.getElementById('tartarugas2');
 const acelerarBtn = document.getElementById('acelerar');
 const freiarBtn = document.getElementById('freiar');
 
-let position = 0;
-let velo = 2;
+
+
+let position = 50;
+let velo = 0;
 let pausar = false;
 let animacaoID;
 
 function animacao() {
     if (!pausar) {
         position -= velo;
-        if (position < -100) {
+        if (position < -30) {
             position = 0
         }
-        tartarugas1.style.transform = `translateX(${position}%) translateY(-50%)`;
-        tartarugas2.style.transform = `translateX(${position}%) translateY(-50%)`;
+        tartarugas1.style.transform = `translateX(${position}%) translateY(-100%)`;
+        tartarugas2.style.transform = `translateX(${position}%) translateY(-100%)`;
     }
 
     animacaoID = requestAnimationFrame(animacao);
@@ -29,7 +31,7 @@ acelerarBtn.addEventListener('click', () => {
         pausar = false;
         animacao();
     }
-    if (velo < 10) {
+    if (velo < 3) {
         velo += 1;
     }
 });
